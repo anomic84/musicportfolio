34c3c239231498d4f1ec450091ff9d74a365003e
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-31 13:55:29 -0700

    made navbar take full screen on mobile

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
       <div className="hidden lg:flex flex-row justify-center items-center">
         <div className="flex">
           <div className="flex flex-1 items-center justify-end">
-   
+
             <p className='lg:px-2 xl:px-3 whitespace-nowrap text-sm text-navtext'><a href="#busride">BUS RIDE DAYDREAM</a></p>
             <p className='lg:px-2 xl:px-3 whitespace-nowrap text-sm text-navtext'><a href="#productions">PRODUCED BY</a></p>
           </div>
@@ -18,7 +18,7 @@ const Navbar = () => {
             <p className='lg:px-2  xl:px-3 whitespace-nowrap lg:text-2xl xl:text-3xl font-climate tracking-widest text-navnametext'><a href="#home">SPENCER CARLSON</a></p>
           </div>
           <div className="flex flex-1 items-center justify-start">
-          <p className='lg:px-2  xl:px-3 whitespace-nowrap text-sm text-navtext'><a href="#about">ABOUT</a></p>
+            <p className='lg:px-2  xl:px-3 whitespace-nowrap text-sm text-navtext'><a href="#about">ABOUT</a></p>
             <p className='lg:px-2  xl:px-3 text-sm text-navtext'><a href="#news">NEWS</a></p>
             <p className='lg:px-2  xl:px-3 text-sm text-navtext'><a href="#media">MUSIC</a></p>
             <p className='lg:px-2  xl:px-3 text-sm text-navtext'><a href="#contact">CONTACT</a></p>
@@ -44,27 +44,36 @@ const Navbar = () => {
         </div>
       </div>
       {/* MOBILE */}
-      <div className="sm:hidden flex justify-between items-center px-4">
-        <h1 className="text-navnametext text-lg text-center">Spencer Carlson</h1>
-        {toggleMenu
-          ? <FaTimes className="text-navnametext" size={27} onClick={() => setToggleMenu(false)} />
-          : <FaBars className="text-navnametext" size={27} onClick={() => setToggleMenu(true)} />}
-        {toggleMenu && (
-          <div className="">
-            <div className="text-navtext dark:text-white sm:text-xl text-center">
-              <p className='px-3 text-sm text-navtext'><a href="#news">NEWS</a></p>
-              <p className='px-3 text-sm text-navtext'><a href="#media">MUSIC</a></p>
-              <p className='px-3 text-sm text-navtext'><a href="#about">ABOUT</a></p>
-              <p className='px-3 text-sm text-navtext'><a href="#contact">CONTACT</a></p>
-              <p className='px-3 text-sm text-navtext'><a href="#productions">PRODUCED BY</a></p>
-              <p className='px-3 text-sm text-navtext'><a href="#busride">BUS RIDE DAYDREAM</a></p>
-              {/* <p><a href="#footer">Contact</a></p> */}
 
+      <div className="sm:hidden flex justify-between items-center px-4 w-full">
+        {toggleMenu
+          ? ''
+          : <h1 className="text-navnametext text-lg  text-center">Spencer Carlson</h1>
+        }
+        <div className="flex ">
+
+          {toggleMenu
+            ? <FaTimes className="text-navnametext" size={27} onClick={() => setToggleMenu((prev) => !prev)} />
+            : <FaBars className="text-navnametext" size={27} onClick={() => setToggleMenu((prev) => !prev)} />
+          }
+          {toggleMenu && (
+            <div className="z-10 h-screen w-[100%] flex justify-center items-center">
+              <div className="text-navtext dark:text-white sm:text-xl text-center flex flex-col justify-center">
+                <p className='py-5 text-xl text-navtext'><a href="#media">MUSIC</a></p>
+                <p className='py-5 text-xl text-navtext'><a href="#about">ABOUT</a></p>
+                <p className='py-5 text-xl text-navtext'><a href="#contact">CONTACT</a></p>
+                <p className='py-5 text-xl text-navtext'><a href="#productions">PRODUCED BY</a></p>
+                <p className='py-5 text-xl text-navtext'><a href="#news">NEWS AND SHOWS</a></p>
+                <p className='py-5 text-xl text-navtext'><a href="#busride">BUS RIDE DAYDREAM</a></p>
+
+
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
+
   )
 }
 
