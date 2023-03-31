3b985a67fe415a61ebbe6650f5248918e2eb5b3b
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-31 16:30:10 -0700

    added songwhips to busride daydream

--- src/containers/Busride/Busride.jsx
+++ src/containers/Busride/Busride.jsx
@@ -6,15 +6,18 @@ const Busride = () => {
   return (
     <section className="bg-navnametext py-10" id="busride">
       <div>
-        <h1 className='text-center font-bungee text-aboutbg text-5xl px-[17px] pb-3'>Bus Ride <br className='md:hidden' />Daydream</h1>
+        <a target="_blank"
+          rel="noopener noreferrer"
+          href="https://songwhip.com/busridedaydream">
+          <h1 className='text-center font-bungee text-aboutbg text-5xl hover:text-[3.25rem] duration-300 px-[17px] pb-3'>
+            Bus Ride <br className='md:hidden' />Daydream
+          </h1>
+        </a>
       </div>
-      {/* <div class>
-        <img src="https://i.imgur.com/yq8vOmx.png" alt="" className="w-[80%]  shadow-lg mx-auto  max-w-[960px] max-h-[620px] " />
-      </div> */}
       <div className='flex flex-col items-center justify-center mx-auto py-4 mb-4 rounded-b-[20px] max-w-[450px] shadow-lg '>
         <iframe className="w-[300px] h-[300px] md:w-[450px] md:h-[450px]" src="https://bandcamp.com/EmbeddedPlayer/album=3992714661/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/track=2677760773/transparent=true/" seamless><a href="https://busridedaydream.bandcamp.com/album/aquamoon" title="aquamoon">Aquamoon by Bus Ride Daydream</a></iframe>
         <p className='p-[17px] w-[300px] md:w-[450px] m-auto font-ubuntu text-sm md:text-md  text-aboutbg'>Bus Ride Daydream is a side project by Spencer Carlson. This project is a sonic experiment mixing element of Pychadelic with Lo-Fi Hip Hop<br />
-        The ultimate goal is both create the magical feeling of nostalgia that Lo-Fi Hip Hop eminates with the essence of surrealism hovering </p>
+          The ultimate goal is both create the magical feeling of nostalgia that Lo-Fi Hip Hop eminates with the essence of surrealism hovering </p>
       </div>
       <div className='flex flex-col sm:flex-row sm:flex-wrap items-center justify-center py-4 bg-navnametext'>
         {busrides.map((busride) => (
